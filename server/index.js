a322367fa27060f5e210e5ae0c2c7ea71360562c
faizanharoon1 <faizanharoon1@outlook.com>
2018-02-07 07:42:30 -0500

    Production build done after billing module AWESOME

--- server/index.js
+++ server/index.js
@@ -27,5 +27,21 @@ app.use(passport.session()); // passport will know to use cookie for Auth.
 require('./routes/authRoutes')(app); // refactored way of calling requiring function in JS
 require('./routes/billingRoutes')(app);
 
+if(process.env.NODE_ENV === 'production'){ // heroku env variable NODE_ENV
+
+// EXPRESS WILL server up production assetrs 
+// like our main.js file or main.css file
+
+app.use(express.static('client/build'));
+
+
+// Express will server index .html file
+// if it doesnt find the route
+const path = require('path');
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+})
+}
+
 const PORT= process.env.PORT || 5000; // heroku dynamic port binding.
 app.listen(PORT);
